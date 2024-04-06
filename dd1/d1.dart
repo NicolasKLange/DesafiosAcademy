918b3e53752b58bd3518a21328b6dab761c97d42
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-04-06 13:41:58 -0300

    Desafio 1 > Finalizado

--- dd1/d1.dart
+++ dd1/d1.dart
@@ -1,3 +1,12 @@
+import 'dart:math';
+
 void main() {
-  
+  List<double> raios = [5, 8, 12, 7.3, 18, 2, 25];
+
+  for(int i = 0; i < raios.length; i++){
+    print("Raio: ${raios[i]}, "
+          "area: ${(pi * raios[i] * raios[i]).toStringAsFixed(2)}, "
+          "perimetro: ${(2 * pi * raios[i]).toStringAsFixed(2)}");
+
+  }
 }

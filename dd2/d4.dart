91ef90e52a900e15b3c294b160f859bbec0cb051
Nicolas Lange <148658117+NicolasKLange@users.noreply.github.com>
2024-04-11 13:33:35 -0300

    Desafio 1 > Finalizado

--- dd2/d4.dart
+++ dd2/d4.dart
@@ -1,133 +1,144 @@
 import 'dart:math' as math;
 
-/// Classe base para representar formas geométricas.
+void main() {
+  final comparador = ComparadorFormasGeometricas();
+
+  final circuloA = Circulo('Circulo A', 3);
+  final circuloB = Circulo('Circulo B', 8);
+  final retanguloA = Retangulo('Retângulo A', 4, 3);
+  final retanguloB = Retangulo('Retângulo B', 19, 11);
+  final trianguloEquilateroA = TrianguloEquilatero('Triângulo Equilátero A', 5);
+  final trianguloEquilateroB = TrianguloEquilatero('Triângulo Equilátero B', 8);
+  final trianguloRetanguloA = TrianguloRetangulo('Triângulo Retângulo A', 4, 3);
+  final trianguloRetanguloB = TrianguloRetangulo('Triângulo Retângulo B', 5, 12);
+  final pentagonoRegularA = PentagonoRegular('Pentágono Regular A', 5);
+  final pentagonoRegularB = PentagonoRegular('Pentágono Regular B', 8);
+  final hexagonoRegularA = HexagonoRegular('Hexágono Regular A', 5);
+  final hexagonoRegularB = HexagonoRegular('Hexágono Regular B', 8);
+
+  final formas = [
+    circuloA,
+    circuloB,
+    retanguloA,
+    retanguloB,
+    trianguloEquilateroA,
+    trianguloEquilateroB,
+    trianguloRetanguloA,
+    trianguloRetanguloB,
+    pentagonoRegularA,
+    pentagonoRegularB,
+    hexagonoRegularA,
+    hexagonoRegularB,
+  ];
+
+  FormaGeometrica maiorArea = comparador.maior(formas, (a, b) => a.area.compareTo(b.area));
+  FormaGeometrica maiorPerimetro =
+  comparador.maior(formas, (a, b) => a.perimetro.compareTo(b.perimetro));
+
+  print('A maior área é ${maiorArea.nome} com ${maiorArea.area.toStringAsFixed(2)}');
+  print('O maior perímetro é ${maiorPerimetro.nome} com ${maiorPerimetro.perimetro.toStringAsFixed(2)}');
+}
+
 abstract class FormaGeometrica {
   final String nome;
 
-  /// Construtor que recebe o [nome] da forma geométrica.
   FormaGeometrica(this.nome);
 
-  /// Retorna a área da forma geométrica.
-  double area();
-
-  /// Retorna o perímetro da forma geométrica.
-  double perimetro();
+  double get area;
+  double get perimetro;
 }
 
-/// Representa a forma geométrica "círculo".
 class Circulo extends FormaGeometrica {
   final double raio;
 
-  /// Construtor que recebe o [nome] e o [raio] do círculo.
   Circulo(String nome, this.raio) : super(nome);
 
   @override
-  double area() => math.pi * math.pow(raio, 2);
+  double get area => math.pi * math.pow(raio, 2);
 
   @override
-  double perimetro() => 2 * math.pi * raio;
+  double get perimetro => 2 * math.pi * raio;
 }
 
-/// Representa a forma geométrica "retângulo".
 class Retangulo extends FormaGeometrica {
-  final double altura;
   final double largura;
+  final double altura;
 
-  /// Construtor que recebe o [nome], [altura] e [largura] do retângulo.
-  Retangulo(String nome, this.altura, this.largura) : super(nome);
-
-  @override
-  double area() => altura * largura;
+  Retangulo(String nome, this.largura, this.altura) : super(nome);
 
   @override
-  double perimetro() => (altura * 2) + (largura * 2);
+  double get area => largura * altura;
+
+  @override
+  double get perimetro => (largura * 2) + (altura * 2);
 }
 
-/// Representa a forma geométrica "quadrado".
-class Quadrado extends Retangulo {
-  /// Construtor que recebe o [nome] e o [lado] do quadrado.
-  Quadrado(String nome, double lado) : super(nome, lado, lado);
-}
-
-/// Representa a forma geométrica "triângulo equilátero".
 class TrianguloEquilatero extends FormaGeometrica {
   final double lado;
 
-  /// Construtor que recebe o [nome] e o [lado] do triângulo equilátero.
   TrianguloEquilatero(String nome, this.lado) : super(nome);
 
   @override
-  double area() => (math.sqrt(3) / 4) * math.pow(lado, 2);
+  double get area => (lado * lado * math.sqrt(3)) / 4;
 
   @override
-  double perimetro() => 3 * lado;
+  double get perimetro => lado * 3;
 }
 
-/// Representa a forma geométrica "triângulo retângulo".
 class TrianguloRetangulo extends FormaGeometrica {
   final double base;
   final double altura;
 
-  /// Construtor que recebe o [nome], [base] e [altura] do triângulo retângulo.
   TrianguloRetangulo(String nome, this.base, this.altura) : super(nome);
 
   @override
-  double area() => (base * altura) / 2;
+  double get area => (base * altura) / 2;
 
   @override
-  double perimetro() => base + altura + math.sqrt(math.pow(base, 2) + math.pow(altura, 2));
+  double get perimetro => base + altura + math.sqrt((base * base) + (altura * altura));
 }
 
-/// Representa a forma geométrica "pentágono regular".
 class PentagonoRegular extends FormaGeometrica {
   final double lado;
 
-  /// Construtor que recebe o [nome] e o [lado] do pentágono regular.
   PentagonoRegular(String nome, this.lado) : super(nome);
 
   @override
-  double area() {
+  double get area {
     final apotema = lado / (2 * math.tan(math.pi / 5));
     return (5 * lado * apotema) / 2;
   }
 
   @override
-  double perimetro() => 5 * lado;
+  double get perimetro => lado * 5;
 }
 
-/// Representa a forma geométrica "hexágono regular".
 class HexagonoRegular extends FormaGeometrica {
   final double lado;
 
-  /// Construtor que recebe o [nome] e o [lado] do hexágono regular.
   HexagonoRegular(String nome, this.lado) : super(nome);
 
   @override
-  double area() => (3 * math.sqrt(3) * math.pow(lado, 2)) / 2;
-
-  @override
-  double perimetro() => 6 * lado;
-}
-
-/// Compara características de formas geométricas.
-class ComparadorFormasGeometricas {
-  /// Retorna a forma geométrica com a maior área, ou [formaA] caso as áreas sejam iguais.
-  FormaGeometrica formaDeMaiorArea(FormaGeometrica formaA, FormaGeometrica formaB) {
-    if (formaA.area() > formaB.area()) {
-      return formaA;
-    } else if (formaB.area() > formaA.area()) {
-      return formaB;
-    }
-    return formaA;
+  double get area {
+    final apotema = lado * math.sqrt(3) / 2;
+    return (3 * math.sqrt(3) * lado * lado) / 2;
   }
 
-  /// Retorna a forma geométrica com o maior perímetro, ou [formaA] caso os perímetros sejam iguais.
-  FormaGeometrica formaDeMaiorPerimetro(FormaGeometrica formaA, FormaGeometrica formaB) {
-    if (formaA.perimetro() > formaB.perimetro()) {
-      return formaA;
-    } else if (formaB.perimetro() > formaA.perimetro()) {
-      return formaB;
+  @override
+  double get perimetro => lado * 6;
+}
+
+class ComparadorFormasGeometricas {
+  T maior<T extends FormaGeometrica>(List<T> formas, int Function(T a, T b) comparador) {
+    if (formas.isEmpty) {
+      throw ArgumentError('A lista de formas não pode ser vazia.');
     }
-    return formaA;
+    T formaMaior = formas[0];
+    for (int i = 1; i < formas.length; i++) {
+      if (comparador(formas[i], formaMaior) > 0) {
+        formaMaior = formas[i];
+      }
+    }
+    return formaMaior;
   }
 }
